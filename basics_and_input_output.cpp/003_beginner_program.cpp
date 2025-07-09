e14aee82b7c67033f97b09b9a289ca0278657594
himanshu27 <himanshupande77@gmail.com>
2025-07-10 01:48:40 +0530

    Q3. Swap two numbers without using a third variable

---
+++ basics_and_input_output.cpp/003_beginner_program.cpp
@@ -0,0 +1,16 @@
+// Q3. Swap two numbers without using a third variable
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int a = 6;
+int b = 27;
+   
+a = a+b;
+b = a-b;
+a = a-b;
+
+cout <<"a = "<<a <<",b = "<<b;
+    return 0;
+}
