e8fcba9c48cb9cda95eeb84e1e06a5e8825e261b
himanshu27 <himanshupande77@gmail.com>
2025-07-10 22:56:05 +0530

    Q4. Check if a number is even or odd

---
+++ basics_and_input_output.cpp/004_beginner_program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter a number"<<endl;
+    cin>>num;
+      if(num%2==0){
+        cout<<num<<" is an even number"<<endl;
+    }
+    else{
+        cout<<num<<"is a odd number";
+    }
+    return 0;
+}
+    
