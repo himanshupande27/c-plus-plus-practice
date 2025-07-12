5ad91450b41e6f64e0d1299079b066b70513fee7
himanshu27 <himanshupande77@gmail.com>
2025-07-12 23:51:33 +0530

    Q10. Find the square and cube of a number

---
+++ basics_and_input_output.cpp/010_beginner_program.cpp
@@ -0,0 +1,13 @@
+// Q10. Find the square and cube of a number
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<<"Enter the number"<<endl;
+    cin>>num;
+    cout<<"square of the "<<num<<" is "<<num*num<<endl;
+    cout<<"cube of "<<num<<" is "<<num*num*num<<endl;
+    return 0;
+}
