f7e1f884d01c6e4b2b8037e0934edffecf31fa94
himanshu27 <himanshupande77@gmail.com>
2025-07-13 21:00:52 +0530

    Q1.  Print numbers from 1 to N

---
+++ loops_and_patterns/011_beginner_program.cpp
@@ -0,0 +1,15 @@
+// Q1.  Print numbers from 1 to N
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<< "Enter a number: ";
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
