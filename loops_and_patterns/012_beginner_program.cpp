4867942e82dece7c4cd9a8a4ae086fb026fba59b
himanshu27 <himanshupande77@gmail.com>
2025-07-13 23:32:11 +0530

    Q2. Print sum of first N natural numbers

---
+++ loops_and_patterns/012_beginner_program.cpp
@@ -0,0 +1,12 @@
+// Q2. Print sum of first N natural numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<< "Enter a first natural number:"<<endl;
+    cin>>n;
+   cout<<" The sum of firstc"<<n<<"natural no is "<<n*(n+1)/2;
+    return 0;
+}
