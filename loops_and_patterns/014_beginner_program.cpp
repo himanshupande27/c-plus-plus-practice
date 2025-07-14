ce77505003fd4578f9a9420b642f29490fa58cfd
himanshu27 <himanshupande77@gmail.com>
2025-07-14 21:22:50 +0530

    Q4. Print all even numbers up to N

---
+++ loops_and_patterns/014_beginner_program.cpp
@@ -0,0 +1,16 @@
+// Q14. Print all even numbers up to N
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: "; 
+    cin>>num;
+    for (int i=1;i<=num;i++)
+    {
+        if(i%2==0)
+        cout<<i<<endl;
+    }
+    return 0;
+}
