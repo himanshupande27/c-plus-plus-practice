87ba8a79d5a229ffa111a6675724285e47b23da4
himanshu27 <himanshupande77@gmail.com>
2025-07-20 23:43:59 +0530

    Q15. Pattern: Inverted triangle

---
+++ loops_and_patterns/025_beginner_program.cpp
@@ -0,0 +1,20 @@
+// Q15. Pattern: Inverted triangle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,z=n;
+    cout<<"enter the number of rows: ";
+    cin>>n;
+    for(int i=n;i>=1;i--){
+        for(int a=i;a>=1;a--){
+            cout<<a<<" ";
+            
+            
+        }
+       cout<<endl;
+    } 
+    return 0;
+
+}
