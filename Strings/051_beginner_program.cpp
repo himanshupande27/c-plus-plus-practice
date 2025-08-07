70db7a2029fd70009abc5eb53ff5c63a50c927db
himanshu27 <himanshupande77@gmail.com>
2025-08-07 22:32:40 +0530

    Q1.  Input and print string

---
+++ Strings/051_beginner_program.cpp
@@ -0,0 +1,12 @@
+// Q1.  Input and print string
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<name<<endl;
+    return 0;
+}
