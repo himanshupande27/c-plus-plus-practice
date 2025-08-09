1ae1b02c3fd79d3c6e9bced08e0e13e1660007d4
himanshu27 <himanshupande77@gmail.com>
2025-08-09 17:30:38 +0530

    Q3. Convert lowercase to uppercase

---
+++ Strings/053_beginner_program.cpp
@@ -0,0 +1,18 @@
+// Q3.  Convert lowercase to uppercase
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout<<"Enter a lowercase string: ";
+    getline(cin,str);
+    for (int i=0;i<str.length();i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i]=str[i]-32;
+        }
+    }
+    cout<<"Uppercase "<<str;
+
+    return 0;
+}
